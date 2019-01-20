8df395fd6b44959fb9615b928f0f3d7c19909e1c
Rene Casco <rene.casco@icloud.com>
2019-01-19 21:57:59 -0700

    save_and_open_page added to to test, tmp cabypara folder configured and gitignore file created

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -4,6 +4,7 @@ class HomePageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
 
+    save_and_open_page
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
