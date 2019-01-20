66acf5477ac7e7c1e8083a8d8eee3887f0829fc3
Rene Casco <rene.casco@icloud.com>
2019-01-20 16:12:56 -0700

    test for error page modified, PersonalSite updated

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -1,6 +1,7 @@
 require './test/test_helper'
 
 class HomePageTest < CapybaraTestCase
+
   def test_user_can_see_the_homepage
     visit '/'
 
@@ -8,4 +9,13 @@ class HomePageTest < CapybaraTestCase
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_user_can_see_error_page
+    visit '/randompage.html'
+
+    save_and_open_page
+    assert_equal 404, page.status_code
+    assert page.has_content?("Page not found.")
+  end
+
 end
