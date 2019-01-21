0f4fada682e1b0eece24b892a304209f72b94fad
Rene Casco <rene.casco@icloud.com>
2019-01-20 17:13:59 -0700

    PersnaSite updated to include rout for /about, corresponding view created.

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -13,7 +13,7 @@ class HomePageTest < CapybaraTestCase
   def test_user_can_see_error_page
     visit '/randompage.html'
 
-    save_and_open_page
+    # save_and_open_page
     assert_equal 404, page.status_code
     assert page.has_content?("Page not found.")
   end
